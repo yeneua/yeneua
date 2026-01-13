f390f12d63555818b18bc4d9a1d128bdaf0def7f
yena <yeneua.dev@gmail.com>
2026-01-13 23:50:40 +0900

    fix: 리드미 형식 수정

--- index.js
+++ index.js
@@ -9,25 +9,22 @@ const parser = new Parser({
 
 ;(async () => {
   try {
-    // 1. 티스토리 RSS 피드 가져오기
     const feed = await parser.parseURL('https://yeneua.tistory.com/rss')
 
-    // 2. 새로운 블로그 포스트 리스트 생성 (HTML 형태)
     let blogList = '<ul>\n'
-    const postCount = Math.min(feed.items.length, 5) // 최신글 5개
+    const postCount = Math.min(feed.items.length, 3)
     for (let i = 0; i < postCount; i++) {
       const { title, link } = feed.items[i]
       blogList += `  <li><a href='${link}' target='_blank'>${title}</a></li>\n`
     }
     blogList += '</ul>'
 
-    // 3. 기존 README.md 파일 읽기
     const readmePath = 'README.md'
     const readmeContent = readFileSync(readmePath, 'utf8')
 
-    // 4. 태그 사이의 내용을 블로그 리스트로 교체 (정규표현식 사용)
     const startTag = ''
     const endTag = ''
+
     const regex = new RegExp(`${startTag}[\\s\\S]*${endTag}`, 'g')
 
     const newReadmeContent = readmeContent.replace(
@@ -35,10 +32,9 @@ const parser = new Parser({
       `${startTag}\n${blogList}\n${endTag}`
     )
 
-    // 5. 수정된 내용으로 README.md 덮어쓰기
     writeFileSync(readmePath, newReadmeContent, 'utf8')
-    console.log('✅ 블로그 포스트 부분만 업데이트 완료!')
+    console.log('블로그 포스팅 업데이트 성공')
   } catch (error) {
-    console.error('❌ 업데이트 중 에러 발생:', error)
+    console.error('업데이트 에러 발생:', error)
   }
 })()
